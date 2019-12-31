d5e464ae005f910990b776637248b4019f1a162c
cyrejorin <cyrejorin@users.noreply.github.com>
2019-12-30 16:53:31 -0800

    Initial Commit

---
+++ CodePath Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodePath Tip Calculator
+//
+//  Created by Cyre Jorin To on 12/30/19.
+//  Copyright © 2019 Cyre Jorin To. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
